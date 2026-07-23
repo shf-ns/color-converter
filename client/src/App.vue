ff5f438a92c3fd94a2d64c63056595b61beb41ca
shf-ns <3104467468@qq.com>
2026-07-24 00:42:19 +0800

    feat(client): 添加侧边栏导航功能并优化布局
    - 在App.vue中添加侧边栏组件，包含工具导航菜单
    - 添加颜色转换器图标资源文件
    - 设置主容器为flex布局以支持侧边栏布局
    - 在base.css中添加全局样式配置，包括背景色和列表样式
    - 重命名sidebar.vue为aside.vue并更新导出名称

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,46 @@
 </script>
 
 <template>
-
+  <div class="sidebar">
+    <h1>工具箱</h1>
+    <ul class="nav">
+      <li class="color-converter">
+        <div class="color-converter-icon">
+          <img src="./assets/img/shouyechangguishouye-copy-copy.png" alt="">
+        </div>
+        <span>所有工具</span>
+      </li>
+    </ul>
+  </div>
+  <div class="main">主题</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.sidebar {
+  width: 20%;
+  height: 100%;
+  padding: 20px;
+  background-color: #fff;
+}
+
+.nav {
+  padding: 5px;
+  margin-top: 30px;
+}
+
+.color-converter {
+  display: flex;
+  align-items: center;
+  gap: 15px;
+}
+
+.color-converter-icon {
+  width: 25px;
+  height: 25px;
+}
+
+.color-converter-icon img {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- client/src/component/index.ts
+++ client/src/component/index.ts
@@ -1,1 +1,1 @@
-export { default as Sidebar } from './sidebar.vue'
+export { default as Aside } from './aside.vue'
